97a125c2b316d64e644b4900bc05c3c5ba168f13
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2025-02-18 23:17:58 -0500

    defined models for Professor and Course

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 class User(Base):
@@ -8,3 +9,34 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False) # no need for unique=True because this is primary key
+    name = Column(String, nullable=False)
+    code = Column(String, index=True, nullable=False, unique=True) # index=True will help fasten query responses
+    description = Column(String)
+    recurring = Column(Boolean, default=False)
+    credit_hours = Column(Integer, nullable=False)
+   
+    # TODO: Update the properties of these; enforce limited options of reqs and campus?; how to make preqs a list of Course objs?
+    prequisites = Column(String)
+    requirement_designation = Column(String)
+    campus = Column(String)
+    # TODO: add department?
+
+    professor_id = Column(Integer, ForeignKey("professors.id"))
+    professor = relationship("Professor", back_populates="courses") # define a bidirectional relationship between courses and professors
+
+
+class Professor(Base):
+    __tablename__ = "professors"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, unique=True)
+    department = Column(String)
+
+    courses = relationship("Course", back_populates="professor")
