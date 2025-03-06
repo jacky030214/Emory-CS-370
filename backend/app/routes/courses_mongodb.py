9af9599ce91821168f114b1b765cc40ddf43daf1
AndrewPark <96x707@gmail.com>
2025-03-06 13:11:31 -0500

    add underbar on api names

--- backend/app/routes/courses_mongodb.py
+++ backend/app/routes/courses_mongodb.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-import schemas, crud, database
+import schemas
+import crud
+import database
 from Functions.Get_Class_byID import get_class_by_id
 
 router = APIRouter()
 
 
-@router.get("/get class by class id")
+@router.get("/get_class_by_class_id")
 def get_Class(class_id: str):
     class_obj = get_class_by_id(class_id)
     if class_obj:
--- backend/app/routes/majorReq.py
+++ backend/app/routes/majorReq.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-import schemas, crud, database
+import schemas
+import crud
+import database
 from Functions.Get_Major_Req_byName import get_major_requirements_by_name
 
 router = APIRouter()
 
-@router.get("/get major requirement by major name")
+
+@router.get("/get_major_requirement_by_major_name")
 def get_MajorReq(major_name: str):
     majorReq = get_major_requirements_by_name(major_name)
     if majorReq:
