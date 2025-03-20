bcdc56f849a7663f4d3d808ffe64605eeea8b32a
AndrewPark <96x707@gmail.com>
2025-03-19 20:38:16 -0400

    api.js fixed

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base API URL
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://127.0.0.1:8000';
 
 // Create axios instance with default config
 const api = axios.create({
