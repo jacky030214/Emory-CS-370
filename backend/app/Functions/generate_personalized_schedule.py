6916637a70100385e511db243697c9c698a36ce8
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2025-04-02 13:47:26 -0400

    refactored code; working on logic

--- backend/app/Functions/generate_personalized_schedule.py
+++ backend/app/Functions/generate_personalized_schedule.py
@@ -1,119 +1,186 @@
 import pandas as pd
+import random
 
+all_requirements = ["Humanities and Arts", "Natural Science", "Quantitative Reasoning", "Social Science", "First Year Seminar", "First Year Writing", "Continuing Communication", "Intercultural Communication", "Race and Ethnicity", "Experience and Application", "Physical Education", "Health"]
+
+class Course:
+    def __init__(self, course_id: str, section: int, crn: int, course_name: str, recurring: str, prereqs: list[str], requirement_designation: list[str], campus: str, description: str, professor: 'Professor'):
+        if not (course_id and section) and not crn: raise AmbiguousCourseError
+        assert recurring in ["fall", "spring", "summer", "fall/spring", None], "recurring must be 'fall', 'spring', 'summer', 'fall/spring', or None"
+        for req in requirement_designation:
+            assert req in all_requirements, f"requirement_designation array values must be one of the following: {all_requirements}"
+        assert campus in ["Emory", "Oxford", None], "campus must be 'Emory', 'Oxford', or None"
+        self.course_id = course_id
+        self.section = section
+        self.crn = crn
+        self.course_name = course_name
+        self.recurring = recurring
+        self.prereqs = prereqs
+        self.requirement_designation = requirement_designation
+        self.campus = campus
+        self.description = description
+        self.professor = professor
+        # TODO: Add meeting times, seats, location, capacity, permission req, credits attributes
+    # TODO: Add is_full method, check_prereqs method 
+
+class Professor:
+    def __init__(self, name: str, email, rmp_rating: float):
+        assert 0 <= rmp_rating <= 5, "rmp_rating must be between 0 and 5"
+        self.name = name
+        self.email = email
+        self.rmp_rating = rmp_rating
+
+class Schedule:
+    def __init__(self, mon: list[Course], tues: list[Course], wed: list[Course], thurs: list[Course], fri: list[Course]):
+        self.mon = mon
+        self.tues = tues
+        self.wed = wed
+        self.thurs = thurs
+        self.fri = fri
+
+class Major:
+    def __init__(self, name: str, requirements: dict[str, list[str]]):
+        self.name = name
+        self.requirements = requirements
+        assert requirements.keys().__contains__("required_courses") and requirements.keys().__contains__("elective1"), "Major must have required_courses and at least a elective1 requirement"
+
+class Preferences:
+   def __init__(self, rmp_score: str | float, ger: list[str], prereqs: str | list[str], campus: str, semester: str, description: str):
+        if isinstance(rmp_score, str): assert rmp_score in ["high", None], "rmp_score must be 'high' or None"
+        else: assert 0 <= rmp_score <= 5, "rmp_score must be a float between 0 and 5"
+        if not isinstance(prereqs, list): assert prereqs in ["high", "low", None], "prereqs must be 'high', 'low', or None or a list of prereqs"
+        assert campus in ["Emory", "Oxford", None], "campus must be 'Emory', 'Oxford', or None"
+        assert semester in ["fall", "spring", "summer", None], "recurring must be 'fall', 'spring', 'summer', or None"
+        self.rmp_score = rmp_score # min rmp score or preference for high vs low score
+        self.ger = ger
+        self.prereqs = prereqs
+        self.campus = campus
+        self.semester = semester
+        self.description = description
+        
 # TODO: Add semester selection to all functions 
-CourseArgumentError = ValueError("You must provide either (course_id and section) or crn.")
-PreferencesError = ValueError("You must provide student preferences.")
+AmbiguousCourseError = ValueError("Course object must have either (course_id and section) or crn.")
+NullCourseError = ValueError("Course object cannot be null.")
 
-# rmp = rate my professor
-def rmp_rating(course_id=None, section=None, crn=None):
-    ratings = pd.read_csv("backend/app/Data/rmp_ratings.csv")
-    ratings["section"] = ratings["section"].astype(str)
-    if crn:
-        return ratings.loc[ratings["crn"] == crn, "rating"].iloc[0] if not ratings.loc[ratings["crn"] == crn].empty else 2.5
-    elif course_id and section:
-        filtered = ratings[(ratings["course_id"] == course_id) & (ratings["section"] == section)]
-        return filtered["rating"].iloc[0] if not filtered.empty else 2.5
-    else:
-        raise CourseArgumentError
+def calculate_suitability(course: Course, preferences: Preferences, include_desc: bool = False) -> float:
+    if not course:
+        raise NullCourseError
+    if not course.crn and not course.course_id and not course.section:
+        raise AmbiguousCourseError
 
-def course_attributes(course_id=None, section=None, crn=None):
-    course_data = pd.read_csv("backend/app/Data/class.csv")
-
-    if crn:
-        course_data = course_data.loc[course_data["crn"] == crn]
-    elif course_id and section:
-        course_data["section"] = "1"
-        course_data = course_data[(course_data["class_id"] == course_id) & (course_data["section"] == section)]
-
-    else:
-        raise CourseArgumentError
+    RMP_score = course.professor.rmp_rating
+    prereq_score = sum(len(req.split("or")) for req in course.prereqs) / len(course.prereqs) if course.prereqs else 0
+    campus_score = 1 if course.campus == preferences.campus else 0
     
-    course_attributes = { # TODO: Add more attributes based on available data
-        "class_name": course_data["class_id"].iloc[0],
-        "recurring": course_data["recurring"].iloc[0],
-        "prereqs": course_data["prereqs"].iloc[0],
-        "requirement_designation": course_data["requirement_designation"].iloc[0],
-        "campus": course_data["campus"].iloc[0],
-        "class_desc": course_data["class_desc"].iloc[0],
-    }
-    return course_attributes
-
-def calculate_suitability(preferences, course_id=None, section=None, crn=None):
-    if not preferences:
-        raise PreferencesError
-    if not crn and not course_id and not section:
-        raise CourseArgumentError
-
-    attributes = course_attributes(course_id=course_id, section=section, crn=crn)
-
-    RMP_score = rmp_rating(course_id=course_id, section=section, crn=crn)
-    GER_score = len(attributes["requirement_designation"].split("with")) if attributes["requirement_designation"] else 0
-    
-    prereq_groups = attributes["prereqs"].split(";") if attributes["prereqs"] and pd.notna(attributes["prereqs"]) else []
-    prereq_score = sum(len(group.split("or")) for group in prereq_groups) / len(prereq_groups) if prereq_groups else 0
-    
-    campus_score = 1 if attributes["campus"] == preferences.get("campus") else 0
-    
-    if preferences["class_desc"]:
-        from sentence_transformers import SentenceTransformer, util
-        model = SentenceTransformer('all-MiniLM-L6-v2')
-        class_desc_embeddings = model.encode(attributes["class_desc"], convert_to_tensor=True)
-        preferred_desc_embeddings = model.encode(preferences["class_desc"], convert_to_tensor=True)
-        similarities = util.pytorch_cos_sim(class_desc_embeddings, preferred_desc_embeddings)
-        class_desc_score = float(similarities[0][0])
-    else:
-        class_desc_score = 0
+    if include_desc:
+        if preferences.description:
+            from sentence_transformers import SentenceTransformer, util
+            model = SentenceTransformer('all-MiniLM-L6-v2')
+            description_embeddings = model.encode(course.description, convert_to_tensor=True)
+            preferred_desc_embeddings = model.encode(preferences.description, convert_to_tensor=True)
+            similarities = util.pytorch_cos_sim(description_embeddings, preferred_desc_embeddings)
+            description_score = float(similarities[0][0])
+        else:
+            description_score = 0
 
     # update scores based on preferences
-    if preferences.get("rmp") == "high":
-        RMP_score *= 2
-    else:
-        RMP_score = 0
+    if isinstance(preferences.rmp_score, str):
+        if preferences.rmp_score == "high":
+            RMP_score *= 2
+        else:
+            RMP_score = 0
+    elif preferences.rmp_score is not None:
+        if RMP_score >= preferences.rmp_score:
+            RMP_score *= 2
+        else:
+            RMP_score = 0
     
-    if preferences.get("ger") == "many":
-        GER_score *= 2
-    else:
-        GER_score = 0
+    GER_score = 0
+    for ger in course.requirement_designation:
+        if ger in preferences.ger:
+            GER_score += 1
 
-    if preferences.get("prereqs") == "few":
+    # TODO: fix this heuristic to use completed prereqs
+    if preferences.prereqs == "few":
         prereq_score = 10 - prereq_score # inverse
         if prereq_score < 0:
             prereq_score = 0 # if more than 10 prereqs then worst score
     else:
         prereq_score = 0
     
-    if preferences.get("campus") == attributes["campus"]:
+    if preferences.campus == course.campus:
         campus_score *= 2
     else:
         campus_score = 0
 
+    # normalize individual attribute scores
     RMP_score /= 5
-    GER_score /= 2
+    GER_score /= len(preferences.ger) if preferences.ger else 0
     prereq_score /= 10
     campus_score /= 2
-    class_desc_score /= 1
+    description_score /= 1
     
-    scores = [RMP_score, GER_score, prereq_score, campus_score, class_desc_score]
+    # TODO: fix final summation to not disregard zero scores and instead disregard None scores in final avg (also fix logic above)
+    scores = [RMP_score, GER_score, prereq_score, campus_score, description_score]
+    print(scores)
     non_zero_scores = [score for score in scores if score > 0]
     suitability_score = sum(non_zero_scores) / len(non_zero_scores) if non_zero_scores else 0
     return suitability_score
 
 def main():
-    # Sample preferences dictionary
-    preferences = {
-        "rmp": "high",
-        "ger": "",
-        "prereqs": "few",
-        "campus": "",
-        "class_desc": "I want to learn about integrals and derivatives."
-    }
-
-    suitability_score = calculate_suitability(
-        preferences=preferences,
-        course_id="Math111",
-        section="1"
+    preferences = Preferences(
+        rmp_score="high",
+        ger=["Humanities and Arts", "Natural Science"],
+        prereqs=["CS170", "CS171"],
+        campus="Emory",
+        semester="fall",
+        description="I would like to learn about data structures and algorithms.",
     )
-    print(f"Suitability Score: {suitability_score}")
+
+    professor = Professor(name="Dr. Smith Wilson", email="smith_wilson@emory.edu", rmp_rating=4.5)
+
+    good_course = Course(
+        course_id="CS253",
+        section=1,
+        crn=None,
+        course_name="0Data Structures and Algorithms",
+        recurring="fall",
+        prereqs=["CS171 or 171Z or CS_OX171"],
+        requirement_designation=["Quantitative Reasoning", "Natural Science"],
+        campus="Emory",
+        description="A third course in Computer Science, focusing on advanced programming. Emphasis is on mastery in the use and implementation of data structures and algorithms for classical programming paradigms, using the Java programming language and object oriented design.",
+        professor=professor,
+    )
+
+    bad_course = Course(
+        course_id="CS224",
+        section=1,
+        crn=None,
+        course_name="0Foundations of Computer Science",
+        recurring="fall/spring",
+        prereqs=["CS170 or CS_OX170", "MATH111 or MATH_OX111)"],
+        requirement_designation=["Quantitative Reasoning"],
+        campus="Oxford",
+        description="An introductory course in the theory of Computer Science, focusing on analysis of discrete structures with applications. Emphasis is on developing familiarity with notation, computational acuity and creative problem solving skills.",
+        professor=professor,
+    )
+
+    almost_good_course = Course(
+        course_id="CS253",
+        section=1,
+        crn=None,
+        course_name="1Data Structures and Algorithms",
+        recurring="fall",
+        prereqs=["CS171 or 171Z or CS_OX171"],
+        requirement_designation=["Quantitative Reasoning"],
+        campus="Emory",
+        description="A third course in Computer Science, focusing on advanced programming. Emphasis is on mastery in the use and implementation of data structures and algorithms for classical programming paradigms, using the Java programming language and object oriented design.",
+        professor=professor,
+    )
+
+    for course in [good_course, bad_course, almost_good_course]:
+        suitability_score = calculate_suitability(course=course, preferences=preferences, include_desc=True)
+        print(f"Suitability Score for {course.course_name}: {suitability_score}")
 
 if __name__ == "__main__":
     main()
