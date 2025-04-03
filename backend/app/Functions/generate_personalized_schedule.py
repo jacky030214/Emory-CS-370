7b3f871c18bf51bac0cd4dabb0315409b85e645b
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2025-04-03 17:18:01 -0400

    refined logic

--- backend/app/Functions/generate_personalized_schedule.py
+++ backend/app/Functions/generate_personalized_schedule.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random
+import math
 
 all_requirements = ["Humanities and Arts", "Natural Science", "Quantitative Reasoning", "Social Science", "First Year Seminar", "First Year Writing", "Continuing Communication", "Intercultural Communication", "Race and Ethnicity", "Experience and Application", "Physical Education", "Health"]
 
@@ -45,13 +46,13 @@ class Major:
         assert requirements.keys().__contains__("required_courses") and requirements.keys().__contains__("elective1"), "Major must have required_courses and at least a elective1 requirement"
 
 class Preferences:
-   def __init__(self, rmp_score: str | float, ger: list[str], prereqs: str | list[str], campus: str, semester: str, description: str):
-        if isinstance(rmp_score, str): assert rmp_score in ["high", None], "rmp_score must be 'high' or None"
-        else: assert 0 <= rmp_score <= 5, "rmp_score must be a float between 0 and 5"
+   def __init__(self, rmp_rating: str | float, ger: list[str], prereqs: str | list[str], campus: str, semester: str, description: str):
+        if isinstance(rmp_rating, str): assert rmp_rating in ["high", None], "rmp_rating must be 'high' or None"
+        else: assert 0 <= rmp_rating <= 5, "rmp_rating must be a float between 0 and 5"
         if not isinstance(prereqs, list): assert prereqs in ["high", "low", None], "prereqs must be 'high', 'low', or None or a list of prereqs"
         assert campus in ["Emory", "Oxford", None], "campus must be 'Emory', 'Oxford', or None"
         assert semester in ["fall", "spring", "summer", None], "recurring must be 'fall', 'spring', 'summer', or None"
-        self.rmp_score = rmp_score # min rmp score or preference for high vs low score
+        self.rmp_rating = rmp_rating # min rmp score or preference for high vs low score
         self.ger = ger
         self.prereqs = prereqs
         self.campus = campus
@@ -68,9 +69,43 @@ def calculate_suitability(course: Course, preferences: Preferences, include_desc
     if not course.crn and not course.course_id and not course.section:
         raise AmbiguousCourseError
 
-    RMP_score = course.professor.rmp_rating
-    prereq_score = sum(len(req.split("or")) for req in course.prereqs) / len(course.prereqs) if course.prereqs else 0
-    campus_score = 1 if course.campus == preferences.campus else 0
+    if preferences.rmp_rating:
+        rmp_rating = course.professor.rmp_rating
+        if isinstance(preferences.rmp_rating, str):
+            if preferences.rmp_rating == "high":
+                rmp_rating /= 5
+            elif preferences.rmp_rating == "low":
+                rmp_rating = 1 - (rmp_rating / 5)
+        else:
+            if rmp_rating >= preferences.rmp_rating:
+                rmp_rating = 1 / (1 + math.exp(-5 * (course.professor.rmp_rating - preferences.rmp_rating))) # sigmoid based heuristic with k=5
+    else:
+        rmp_rating = None
+
+    if preferences.prereqs:
+        if isinstance(preferences.prereqs, str):
+            prereq_score = sum(len(req.split("or")) for req in course.prereqs) / len(course.prereqs) if course.prereqs else 0
+            if preferences.prereqs == "low":
+                prereq_score = 10 - prereq_score # inverse
+            else:
+                prereq_score = 0
+            prereq_score /= 10
+        else:
+            prereq_score = 0
+            for completed_req in preferences.prereqs:
+                for course_req in course.prereqs:
+                    course_req = [req.strip() for req in course_req.split("or")]
+                    if completed_req in course_req:
+                        prereq_score += 1
+                        break # move on to next completed_req
+            prereq_score /= len(course.prereqs)
+    else:
+        prereq_score = None
+
+    if preferences.campus:
+        campus_score = 1 if course.campus == preferences.campus else 0
+    else:
+        campus_score = None
     
     if include_desc:
         if preferences.description:
@@ -81,55 +116,28 @@ def calculate_suitability(course: Course, preferences: Preferences, include_desc
             similarities = util.pytorch_cos_sim(description_embeddings, preferred_desc_embeddings)
             description_score = float(similarities[0][0])
         else:
-            description_score = 0
-
-    # update scores based on preferences
-    if isinstance(preferences.rmp_score, str):
-        if preferences.rmp_score == "high":
-            RMP_score *= 2
-        else:
-            RMP_score = 0
-    elif preferences.rmp_score is not None:
-        if RMP_score >= preferences.rmp_score:
-            RMP_score *= 2
-        else:
-            RMP_score = 0
-    
-    GER_score = 0
-    for ger in course.requirement_designation:
-        if ger in preferences.ger:
-            GER_score += 1
-
-    # TODO: fix this heuristic to use completed prereqs
-    if preferences.prereqs == "few":
-        prereq_score = 10 - prereq_score # inverse
-        if prereq_score < 0:
-            prereq_score = 0 # if more than 10 prereqs then worst score
+            description_score = None
     else:
-        prereq_score = 0
-    
-    if preferences.campus == course.campus:
-        campus_score *= 2
+        description_score = None
+        
+    if preferences.ger:
+        ger_score = 0
+        for ger in course.requirement_designation:
+            if ger in preferences.ger:
+                ger_score += 1
+        ger_score /= len(preferences.ger)
     else:
-        campus_score = 0
+        ger_score = None
 
-    # normalize individual attribute scores
-    RMP_score /= 5
-    GER_score /= len(preferences.ger) if preferences.ger else 0
-    prereq_score /= 10
-    campus_score /= 2
-    description_score /= 1
-    
-    # TODO: fix final summation to not disregard zero scores and instead disregard None scores in final avg (also fix logic above)
-    scores = [RMP_score, GER_score, prereq_score, campus_score, description_score]
+    scores = [rmp_rating, ger_score, prereq_score, campus_score, description_score]
     print(scores)
-    non_zero_scores = [score for score in scores if score > 0]
-    suitability_score = sum(non_zero_scores) / len(non_zero_scores) if non_zero_scores else 0
+    preference_scores = [score for score in scores if score is not None]
+    suitability_score = sum(preference_scores) / len(preference_scores) if preference_scores else 0
     return suitability_score
 
 def main():
     preferences = Preferences(
-        rmp_score="high",
+        rmp_rating="high",
         ger=["Humanities and Arts", "Natural Science"],
         prereqs=["CS170", "CS171"],
         campus="Emory",
@@ -145,7 +153,7 @@ def main():
         crn=None,
         course_name="0Data Structures and Algorithms",
         recurring="fall",
-        prereqs=["CS171 or 171Z or CS_OX171"],
+        prereqs=["CS171 or CS171Z or CS_OX171"],
         requirement_designation=["Quantitative Reasoning", "Natural Science"],
         campus="Emory",
         description="A third course in Computer Science, focusing on advanced programming. Emphasis is on mastery in the use and implementation of data structures and algorithms for classical programming paradigms, using the Java programming language and object oriented design.",
@@ -158,7 +166,7 @@ def main():
         crn=None,
         course_name="0Foundations of Computer Science",
         recurring="fall/spring",
-        prereqs=["CS170 or CS_OX170", "MATH111 or MATH_OX111)"],
+        prereqs=["CS170 or CS_OX170", "MATH111 or MATH_OX111"],
         requirement_designation=["Quantitative Reasoning"],
         campus="Oxford",
         description="An introductory course in the theory of Computer Science, focusing on analysis of discrete structures with applications. Emphasis is on developing familiarity with notation, computational acuity and creative problem solving skills.",
@@ -179,7 +187,7 @@ def main():
     )
 
     for course in [good_course, bad_course, almost_good_course]:
-        suitability_score = calculate_suitability(course=course, preferences=preferences, include_desc=True)
+        suitability_score = calculate_suitability(course=course, preferences=preferences, include_desc=False)
         print(f"Suitability Score for {course.course_name}: {suitability_score}")
 
 if __name__ == "__main__":
