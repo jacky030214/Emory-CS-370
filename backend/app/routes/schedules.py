eac0a208f819260907499d743477d38f00dc48ad
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2025-04-22 04:05:53 -0400

    added endpoint for topk

--- backend/app/routes/schedules.py
+++ backend/app/routes/schedules.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 import schemas, crud, database
 from Functions.Generate_Semester_Schedule_byMajor import generate_full_schedule, convert_schedule_to_obj, add_GER_course, generate_future_schedule, Generate_Schedule_withTime
+from Functions.generate_personalized_schedule import get_top_k_courses
 
 router = APIRouter()
 
@@ -54,3 +55,22 @@ def generate_Future_schedule(major_name: str, startingSem: str = "Fall" , starti
         return future_schedule
     else:
         return {"Failed to generate a schedule."}
+    
+@router.post("/get_top_k")
+def get_top_k(all_schedules: list[dict], preferences: dict, k: int = 5, undergraduate_only = True, collection_name="all_courses"):
+    try:
+        topk = get_top_k_courses(all_schedules, preferences, k, undergraduate_only, collection_name)
+        return topk
+    except Exception as e:
+        return {f"Failed to generate a schedule. Error: {str(e)}"}
+"""
+preferences = {
+    "rmp_rating": 4.5,
+    "ger": ["Continued Communication", "Quantitative Reasoning"],
+    "taken": ["CS171", "MATH111"],
+    "campus": "Oxford" or "Emory",
+    "semester": "fall" or "spring",
+    "description": "I want to learn about AI",
+    "times": ["MW 1:00pm-2:15pm", "F 1pm-2pm"], # times when user is BUSY
+} # if not preference DEFAULT VALUE IS NONE FOR ALL EXCEPT FOR GER AND TAKEN ([] for these two)
+"""
